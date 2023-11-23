b7d98205ca33efaf404231e40dd1c266ed1c6c03
sijin <sijincag@gmail.com>
2023-11-24 02:10:03 +0800

    add sort to airtable request during render. ready for deployment

--- Feed Eat Treat Repeat/src/Airtable.js
+++ Feed Eat Treat Repeat/src/Airtable.js
@@ -1,7 +1,7 @@
 const baseId = 'app0oXVuGeHq2HRYJ';
 const tableName = 'tbl7xnuDoU34SL2df';
 const apiKey = import.meta.env.VITE_MY_KEY;
-const baseURL = `https://api.airtable.com/v0/${baseId}/${tableName}`;
+const baseURL = `https://api.airtable.com/v0/${baseId}/${tableName}?sort%5B0%5D%5Bfield%5D=Entry+ID.&sort%5B0%5D%5Bdirection%5D=asc`;
 
 export const fetchEntries = () => {
   return fetch(`${baseURL}`, {
